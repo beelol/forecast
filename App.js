def9409d6fb93cbcab3fe066b317398774ebfae3
Bilal Itani <bilalitani1@gmail.com>
2018-10-25 08:47:42 -0400

    Add tab navigation between pages with dummy page

--- App.js
+++ App.js
@@ -1,31 +1,14 @@
 import Temperature from './src/Temperature'
-import Location from './src/Location'
+import FiveDayForecast from './src/FiveDayForecast'
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import OpenWeatherMap from './src/OpenWeatherMap';
+import {TabNavigator} from 'react-navigation';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+export default TabNavigator({
+    TODAY: Temperature,
+    'FIVE-DAY': FiveDayForecast
 });
-
-type Props = {};
-export default class App extends Component<Props> {
-    render() {
-        return (
-          <Temperature />
-    );
-  }
-}
-
-// <View style={styles.container}>
-//   <Text style={styles.welcome}>Welcome to React Native!</Text>
-//   <Text style={styles.instructions}>To get started, edit App.js</Text>
-//   <Text style={styles.instructions}>{instructions}</Text>
-// </View>
-// {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
 
 const styles = StyleSheet.create({
   container: {
