a88663e367208874d23b4ea91bb582a51318c404
Bilal Itani <bilalitani1@gmail.com>
2018-10-25 08:35:26 -0400

    Switch temperature to use imperial units

--- src/OpenWeatherMap.js
+++ src/OpenWeatherMap.js
@@ -4,7 +4,7 @@ import "isomorphic-fetch";
 
 export default class OpenWeatherMap {
     getWeather(latitude, longitude) {
-        let url = `${OWMConsts.APIRoot}lat=${latitude}&lon=${longitude}&appid=${OWMConsts.key}`;
+        let url = `${OWMConsts.APIRoot}lat=${latitude}&lon=${longitude}&appid=${OWMConsts.key}&units=imperial`;
 
         return fetch(url).then((response) => {
             return new Promise(function (resolve) {
